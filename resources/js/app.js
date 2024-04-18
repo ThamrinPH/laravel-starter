f5f1deba13635b5da2b8f5b685ebfcfadfcbfb8c
Husni <husni.sosnet@gmail.com>
2024-04-18 08:59:02 +0700

    install adminlte

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,7 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCekx3R3xbf1x0ZFRMY1hbRXFPMyBoS35RckVnWXpecXdRR2dYUkJ2');
