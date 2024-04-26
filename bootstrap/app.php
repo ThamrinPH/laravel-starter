dafae35cfac97d1bfb91738eeb531fe05c94c788
Husni <husni.sosnet@gmail.com>
2024-04-26 17:33:19 +0700

    install recommended library for yajra datatable export

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'PDF' => Barryvdh\Snappy\Facades\SnappyPdf::class,
+            'SnappyImage' => Barryvdh\Snappy\Facades\SnappyImage::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
+    Barryvdh\Snappy\ServiceProvider::class,
+    Yajra\DataTables\ExportServiceProvider::class   
 ];
