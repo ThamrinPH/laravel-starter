b3f1e71751c75030505cd49b15bcc52eefea6b8f
Husni <husni.sosnet@gmail.com>
2024-04-29 09:19:22 +0700

    remove unused role and permission assignment

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -31,19 +31,5 @@ class UserSeeder extends Seeder
             'password' => Hash::make('admin-password'),
             'remember_token' => Str::random(10),
         ]);
-
-        $role = Role::findByName('super-admin');
-
-        if( !empty($role) ) $su->assignRole($role);
-
-        $viewPermission = Permission::findByName('branch-view');
-        if( !empty($viewPermission->id) ) $viewPermission->assignRole($role);
-        $createPermission = Permission::findByName('branch-create');
-        if( !empty($createPermission->id) ) $createPermission->assignRole($role);
-        $updatePermission = Permission::findByName('branch-update');
-        if( !empty($updatePermission->id) ) $updatePermission->assignRole($role);
-        $deletePermission = Permission::findByName('branch-delete');
-        if( !empty($deletePermission->id) ) $deletePermission->assignRole($role);
-        
     }
 }
