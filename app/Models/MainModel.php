a9c893fb6f93003b4b970e1642549fe619ee48a7
Husni <husni.sosnet@gmail.com>
2024-04-29 09:19:51 +0700

    add setting default values

--- app/Models/MainModel.php
+++ app/Models/MainModel.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MainModel extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, HasUuids;
+
+    public $fillable = [];
+    public $validations = [];
+
+    public function getValidations()
+    {
+        return $this->validations;
+    }
+
+    public function setDefaultValues($request = []): Array
+    {
+        return $request;
+    }
 }
