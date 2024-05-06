4a3671b4d3b55ca1966b82328fcbce169f8b2af7
Husni <husni.sosnet@gmail.com>
2024-05-06 10:11:12 +0700

    refactor component can handle error from ajax request

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,11 +1,16 @@
-@props(['disabled' => false, 'type' => 'text', 'class' => '', 'id' => '', 'name' => '', 'placeholder' => 'Your input here', 'value'])
+@props(['disabled' => false, 'type' => 'text', 'class' => '', 'id' => '', 'name' => '', 'placeholder' => 'Your input here', 'value', 'prepend' => false, 'append' => false, 'iconClass'])
 
-<input 
-type="{{ $type }}" 
-class="form-control {{ $class }}" 
-id="{{ $name ?? $id }}" 
-name="{{ $name }}" 
-placeholder="{{ $placeholder }}"
-{{ $disabled ? 'disabled' : '' }}
-value="{{ $value ?? null }}"
->
+<div class="input-group">
+    @if($prepend)
+    <div class="input-group-prepend">
+        <span class="input-group-text"><i id="icon-{{ $name ?? '' }}" class="{{ $iconClass ?? 'fas fa-check' }}"></i></span>
+    </div>
+    @endif
+    <input type="{{ $type }}" class="form-control {{ $class }}" id="{{ $name ?? $id }}" name="{{ $name }}" placeholder="{{ $placeholder }}"{{ $disabled ? 'disabled' : '' }}value="{{ $value ?? null }}">
+    @if($append)
+    <div class="input-group-append">
+        <span class="input-group-text"><i id="icon-{{ $name ?? '' }}" class="{{ $iconClass ?? 'fas fa-check' }}"></i></span>
+    </div>
+    @endif
+</div>
+<label {{ $attributes->merge(['id' => 'error-'.$name, 'class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}/>
--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,3 +1,5 @@
 @props(['name', 'id', 'cols', 'rows', 'class', 'value', 'placeholder', 'value', 'disabled'])
 
 <textarea name="{{ $name }}" id="{{ $name ?? $id }}" cols="{{ $cols ?? 30 }}" rows="{{ $rows ?? 10 }}" class="form-control" placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }}>{{ $value ?? $slot }}</textarea>
+
+<label {{ $attributes->merge(['id' => 'error-'.$name, 'class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}/>
